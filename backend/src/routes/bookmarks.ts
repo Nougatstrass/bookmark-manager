18d668b9f9df4e5cbec3fd51bc057f28c0ddf440
Susannah Lindh <mail@sannah.se>
2025-07-28 17:21:37 +0200

    TS setup and server request handling

--- backend/src/routes/bookmarks.ts
+++ backend/src/routes/bookmarks.ts
@@ -1,10 +1,18 @@
 
 import { Router } from 'express';
+import { db } from "../db";
+import { bookmarks } from '../db/schema';
 
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.json({ message: "GET /bookmarks works!" });
+router.get("/", async (req, res)=> {
+    try {
+        const result = await db.select().from (bookmarks);
+        res.json(result);
+    } catch(err) {
+        console.error("Failed to fetch bookmarks", err);
+        res.status(500).json({ error: "Failed to fetch bookmarks" });
+    }
 });
 
 export default router;
