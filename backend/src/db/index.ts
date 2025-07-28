99e1a9ddba724c1174309e131bdee0fc40fd6a0a
Susannah Lindh <mail@sannah.se>
2025-07-28 22:03:19 +0200

    Connection string default

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -5,7 +5,7 @@ import * as schema from "./schema";
 import "dotenv/config";
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL ?? "postgres://localhost:5432/bookmarks"
 });
 
 export const db = drizzle(pool, { schema });
