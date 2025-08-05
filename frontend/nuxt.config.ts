fb7d0de2aa790a818ab529dd457bdc4088eb7d43
Susannah Lindh <mail@sannah.se>
2025-08-05 14:08:54 +0200

    New frontend setup

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['@/assets/css/tailwind.css'],
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
